b8b9c721b750c471f990df4c977fe31ca4814338
Djordje <djsimovic@gmail.com>
2021-03-13 17:09:43 +0100

    feat: orderList test

--- client/src/components/GetStarted/getStarted.test.js
+++ client/src/components/GetStarted/getStarted.test.js
@@ -1,12 +1,12 @@
 import GetStarted from './getStarted';
 import React from 'react';
 import '@testing-library/jest-dom';
-import { BrowserRouter as Router, Route, MemoryRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+
 import userEvent from '@testing-library/user-event';
 import App from '../../App';
-import DishForm from '../DishForm/dishForm';
+
 describe('getStarted page', () => {
   test('button navigates correctly', () => {
     render(
@@ -14,6 +14,7 @@ describe('getStarted page', () => {
         <App />
       </Router>
     );
+
     const bottomNavFirstPage = screen.getByTestId('bottomNavAction2');
     userEvent.click(bottomNavFirstPage);
     const getStartedButton = screen.getByTestId('getStartedButton');
