87d2acd70db067d2e13db0a365a60d921decc203
Mdods <mattdodsworth@yahoo.co.uk>
2021-03-16 17:17:46 +0000

    forgot to save

--- client/src/components/MenuList/menuList.tsx
+++ client/src/components/MenuList/menuList.tsx
@@ -25,20 +25,3 @@ const MenuList: FunctionComponent<menuListProps> = ({menus}) => {
 }
 
 export default MenuList;
-
-// import MenuItemList from '../MenuItemList/menuItemList';
-// function MenuList({ menus }) {
-//   if (menus) {
-//     console.log('menus', menus);
-//     return (
-//       <div data-testid="menuList" className="menuList">
-//         {menus.map((menu) => (
-//           <MenuItemList key={menu.id} menu={menu} />
-//         ))}
-//       </div>
-//     );
-//   } else {
-//     return <p>HELLO</p>;
-//   }
-// }
-// export default MenuList;
