1ccc89d4e17a09f7045112275213834ddc54a7ce
Djordje <djsimovic@gmail.com>
2021-03-17 21:11:25 +0100

    fix: getStarted page style

--- client/src/components/GetStarted/getStarted.tsx
+++ client/src/components/GetStarted/getStarted.tsx
@@ -4,32 +4,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     '& > *': {
+//       margin: theme.spacing(1),
+//     },
+//   },
+// }));
 
 function GetStarted() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
     <div data-testid="getStarted" className="container-home">
-      <style>
+      {/* <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap');
-      </style>
-      <div>Let's cook!</div>
-      <div className={classes.root}>
+      </style> */}
+      <div className='text-letsstartContainer'>
+        <h1 className='text-letsstart'>Let's cook!</h1>
+        </div>
+      <div className='button-getStartedContainer'>
         <Button
+        className='button-getStarted'
           data-testid="getStartedButton"
           variant="contained"
           color="secondary"
           component={Link}
           to="/add_dish"
-          style={{ background: 'black', marginTop: '550px' }}
+        
         >
           Get started
         </Button>
